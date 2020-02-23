9f21c3beec2b60cd5bceebcf7eb0e72eba22d4b4
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-02-23 11:46:46 -0500

    added weights to graph + production build

---
+++ build/precache-manifest.dbf7a33347c04edf893ddde4bfbb2a35.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa4caec81c998b032ec86b26d2f9fb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a0fee2f3fd6a5b57406",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "39630febae9457910796",
+    "url": "/static/css/main.c4310edc.chunk.css"
+  },
+  {
+    "revision": "2a0fee2f3fd6a5b57406",
+    "url": "/static/js/2.5fbe8233.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.5fbe8233.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39630febae9457910796",
+    "url": "/static/js/main.30e35757.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "91e036487e5db27c11b882f24694e208",
+    "url": "/static/media/weight.91e03648.svg"
+  }
+]);
